4798bdec83f90b996daaf7dfc8abd345c2623a17
Karol Walasek <coreconviction@gmail.com>
2020-07-29 18:31:31 +0200

    Added SystemState unit tests

---
+++ tests/SystemState.test.js
@@ -0,0 +1,38 @@
+const { SystemState, Module } = require('..');
+
+module.exports = async function (test) {
+	await test('SystemState', t => {
+		const system = new SystemState();
+
+		t.test('Class resolving', t => {
+			t.test('Prevents use of invalid types as class names', t => {
+				t.throws(() => system.resolveClass(123), /not a string/);
+				t.throws(() => system.resolveClass({}), /not a string/);
+				t.throws(() => system.resolveClass([]), /not a string/);
+			});
+
+			class MyModule extends Module {}
+
+			t.test('Throws if trying to resolve an unknown class', t => {
+				t.throws(() => system.resolveClass(MyModule), /MyModule/);
+			});
+
+			system.addModuleClass(MyModule);
+
+			t.test('Resolves if provided a class or class name', t => {
+				t.ok(system.resolveClass(MyModule) == MyModule);
+				t.ok(system.resolveClass('MyModule') == MyModule);
+			});
+
+			system.addModuleClass(MyModule, 'Alias');
+
+			t.test('Resolves if used an alias', t => {
+				t.ok(system.resolveClass('Alias') == MyModule);
+			});
+
+			t.test('Throws if used an unknown alias or name', t => {
+				t.throws(() => system.resolveClass('SomeModule'), /SomeModule/);
+			});
+		});
+	});
+};
